56a2b6a23d07008404d5a0bfb7b3319b6b580e5b
Raphael Parree <rparree@edc4it.com>
2017-08-25 18:15:34 +0200

    added import of BrowserAnimationsModule for the updated toaster

--- src/app/app.modules.ts
+++ src/app/app.modules.ts
@@ -1,10 +1,11 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {NgModule} from "@angular/core";
 import {ToasterModule} from "angular2-toaster/angular2-toaster";
 
 @NgModule({
-    imports:      [BrowserModule, FormsModule, ToasterModule ],
+    imports:      [BrowserModule, FormsModule, ToasterModule, BrowserAnimationsModule ],
     bootstrap:    [],
     declarations: []
 })
