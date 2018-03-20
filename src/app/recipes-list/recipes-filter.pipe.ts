cf1657f974609ca8aec3604ef5697e75d05e7057
vagrant <vagrant@student.classroom>
2018-03-20 18:49:12 +0100

    finished lab2

--- src/app/recipes-list/recipes-filter.pipe.ts
+++ src/app/recipes-list/recipes-filter.pipe.ts
@@ -5,8 +5,8 @@ import {Recipe} from "../recipe-service/recipe";
 export class RecipeFilterPipe implements PipeTransform {
 
     transform(list: Recipe[], filter: string): ReadonlyArray<Recipe> {
-
         return list.filter( (recipe) => (recipe.title.toUpperCase().indexOf(filter.toUpperCase()) !== -1));
+
     }
 
 }
--- src/app/recipes-list/recipes-list.component.ts
+++ src/app/recipes-list/recipes-list.component.ts
@@ -8,4 +8,10 @@ import {Recipe} from "../recipe-service/recipe";
     template: require("./recipes-list.component.html")})
 export class RecipesListComponent {
     recipes = testData.sort((r1: Recipe, r2: Recipe) => r2.datePublished < r1.datePublished ? -1 : 1);
+
+    myFilter: string;
+
+    onKey(value: string) {
+        this.myFilter += "" + value;
+    }
 }
